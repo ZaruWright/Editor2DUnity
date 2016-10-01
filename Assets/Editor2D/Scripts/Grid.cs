0fd72419990d8d63b577df5c0ad28a0c29119fc4
ZaruWright <samuel_gse@hotmail.com>
2016-10-01 18:45:55 +0100

    Grid done

---
+++ Assets/Editor2D/Scripts/Grid.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+    using UnityEngine;
+    using System.Collections;
+
+    [ExecuteInEditMode]
+    public class Grid : MonoBehaviour {
+
+        public float width    = 32.0f;
+        public float height   = 32.0f;
+        public int numColumns = 20;
+        public int numRows    = 20;
+
+        void OnDrawGizmos()
+        {
+            Vector3 pos = new Vector3(0, 0, 0);
+
+            float halfCanvasWidth = (numColumns * width)/2;
+            float halfCanvasHeight = (numRows * height)/2;
+
+            // Draw Horizontal lines
+            for (float y = pos.y - halfCanvasHeight; y <= pos.y + halfCanvasHeight; y += height)
+            {
+                Gizmos.DrawLine(new Vector3(-halfCanvasWidth, (y / height) * height, 0.0f),
+                                new Vector3(halfCanvasWidth, (y / height) * height, 0.0f));
+            }
+            
+            // Draw Vertical lines
+            for (float x = pos.x - halfCanvasWidth; x <= pos.x + halfCanvasWidth; x += width)
+            {
+                Gizmos.DrawLine(new Vector3((x / width) * width, -halfCanvasHeight, 0.0f),
+                                new Vector3((x / width) * width, halfCanvasHeight, 0.0f));
+            }
+        }
+    }
+#endif
---
+++ Assets/Editor2D/Scripts/GridEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor (typeof(Grid))]
+public class GridEditor : Editor{
+
+    Grid grid;
+
+    public void OnEnable()
+    {
+        grid = (Grid)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+
+        grid.width= EditorGUILayout.FloatField("Grid Width", grid.width);
+
+        grid.height = EditorGUILayout.FloatField("Grid Height", grid.height);
+
+        grid.numColumns = EditorGUILayout.IntField("Number of Columns", grid.numColumns);
+
+        grid.numRows = EditorGUILayout.IntField("Number of Rows", grid.numRows);
+
+        SceneView.RepaintAll();
+    }
+}
